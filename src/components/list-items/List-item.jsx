d5b77ea3cf1c315f14e0ec88a78f4bb7e2a11045
asad-24 <chaudharyasad.370@gmail.com>
2023-09-06 00:03:33 -0700

    before searching

--- src/components/list-items/List-item.jsx
+++ src/components/list-items/List-item.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import "./list-item.css";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-import { OverlayTrigger, Popover } from "react-bootstrap";
+
+
 const ListItem = ({ item }) => {
   let completeTitle = item?.snippet?.title;
   console.log(item?.channelTitle);
 
-  const popoverContent = (
-    <Popover id="popover-title">
-      <Popover.Body>{completeTitle}</Popover.Body>
-    </Popover>
-  );
+
+  const animation=()=>{
+    const card = document.getElementById('animated-card');
+    card.style.transform = 'scale(1)';
+  }
 
   return (
     <>
     
-      <div className="card mt-5  mb-1 fullCard" >
+      <div className="card mt-5  mb-1 fullCard " onMouseLeave={animation} id="animated-card">
         <img
           src={item?.snippet?.thumbnails?.medium?.url}
           className="card-img-top"
@@ -28,6 +29,10 @@ const ListItem = ({ item }) => {
               <h5 className="title">{completeTitle}</h5>
 
               <h6 className="channe-name">{item?.snippet?.channelTitle}</h6>
+              <div className="watchAndAdd d-flex justify-content-between align-items-center mt-3 ">
+              <button type="button" class="btn  btn-outline-secondary onHoverBtn">Watch later</button>
+              <button type="button" class="btn btn-outline-secondary onHoverBtn ms-2">Add to queue</button>
+              </div>
             </div>
           </div>
         </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,72 +1,143 @@
-import React, { useState } from 'react'
-import shortIcon from '../../assests/short-video-icon.png'
-import subscribeIcon from '../../assests/subscription.png'
-import historyIcon from '../../assests/history-icon.png'
-import homeIcon from '../../assests/home-icon.png'
-import trendingIcon from '../../assests/trending.png'
-import musicIcon from '../../assests/music-icon.png'
-import gameIcon from '../../assests/game-icon.png'
-import newsIcon from '../../assests/new-icon.png'
-import sportIcon from '../../assests/sport-icon.png'
-import browserIcon from '../../assests/browser-icon.png'
-import libraryIcon from '../../assests/library-icon.png'
-
+import React, { useState } from "react";
+import shortIcon from "../../assests/short-video-icon.png";
+import subscribeIcon from "../../assests/subscription.png";
+import historyIcon from "../../assests/history-icon.png";
+import homeIcon from "../../assests/home-icon.png";
+import trendingIcon from "../../assests/trending.png";
+import musicIcon from "../../assests/music-icon.png";
+import gameIcon from "../../assests/game-icon.png";
+import newsIcon from "../../assests/new-icon.png";
+import sportIcon from "../../assests/sport-icon.png";
+import browserIcon from "../../assests/browser-icon.png";
+import libraryIcon from "../../assests/library-icon.png";
 
 function Sidebar() {
-   
   return (
- 
-<div>
-    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-      <div className= 'slider-on-off'>
-  
-  <ul>
-  <li className="fw-bold "><img src={homeIcon} alt="icon"className="short-icon me-4" />Home</li>
-    <li className="fw-bold mt-2"><img src={shortIcon} alt="icon"className="short-icon me-4 " />Short</li>
-    <li className="fw-bold mt-2"><img src={subscribeIcon} alt="icon"className="short-icon me-4 " />Subscription</li>
-   
-  </ul>
-  <hr />
-  <ul>
-  <li className="fw-bold "><img src={libraryIcon} alt="icon" className="short-icon  me-4 " />Library</li>
-  <li className="fw-bold mt-3"><img src={historyIcon} alt="icon" className="short-icon me-4 " />History</li>
-  </ul>
-  <hr />
-<ul>
-
-  Sign in to like videos, comment, and subscribe.
-  <button type="button" className="btn mt-3" id="signIn"><i className="bi bi-person-circle me-2"></i>Sign in</button>
-</ul>
-<hr />
-<ul>
-<h6 className="explore">Explore</h6>
-<li className="fw-bold "><img src={trendingIcon} alt="icon" className="short-icon trending-icon  me-4 " />Trending</li>
-<li className="fw-bold "><img src={musicIcon} alt="icon" className="short-icon trending-icon  me-4 " />Music</li>
-<li className="fw-bold "><img src={gameIcon} alt="icon" className="short-icon trending-icon  me-4 " />Game</li>
-<li className="fw-bold "><img src={newsIcon} alt="icon" className="short-icon trending-icon  me-4 " />News</li>
-<li className="fw-bold "><img src={sportIcon} alt="icon" className="short-icon trending-icon  me-4 " />Sport</li>
-
-</ul>
-<hr />
-<ul>
-<li className="fw-bold "><img src={browserIcon} alt="icon" className="short-icon trending-icon  me-4 " />Browse Channel</li>
-</ul>
- 
-
-
-</div>
+    <div>
+      <div
+        class="offcanvas offcanvas-start"
+        data-bs-scroll="true"
+        tabindex="-1"
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+            Backdrop with scrolling
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <div className="slider-on-off">
+            <ul>
+              <li className="fw-bold ">
+                <img src={homeIcon} alt="icon" className="short-icon me-4" />
+                Home
+              </li>
+              <li className="fw-bold mt-2">
+                <img src={shortIcon} alt="icon" className="short-icon me-4 " />
+                Short
+              </li>
+              <li className="fw-bold mt-2">
+                <img
+                  src={subscribeIcon}
+                  alt="icon"
+                  className="short-icon me-4 "
+                />
+                Subscription
+              </li>
+            </ul>
+            <hr />
+            <ul>
+              <li className="fw-bold ">
+                <img
+                  src={libraryIcon}
+                  alt="icon"
+                  className="short-icon  me-4 "
+                />
+                Library
+              </li>
+              <li className="fw-bold mt-3">
+                <img
+                  src={historyIcon}
+                  alt="icon"
+                  className="short-icon me-4 "
+                />
+                History
+              </li>
+            </ul>
+            <hr />
+            <ul>
+              Sign in to like videos, comment, and subscribe.
+              <button type="button" className="btn mt-3" id="signIn">
+                <i className="bi bi-person-circle me-2"></i>Sign in
+              </button>
+            </ul>
+            <hr />
+            <ul>
+              <h6 className="explore">Explore</h6>
+              <li className="fw-bold ">
+                <img
+                  src={trendingIcon}
+                  alt="icon"
+                  className="short-icon trending-icon  me-4 "
+                />
+                Trending
+              </li>
+              <li className="fw-bold ">
+                <img
+                  src={musicIcon}
+                  alt="icon"
+                  className="short-icon trending-icon  me-4 "
+                />
+                Music
+              </li>
+              <li className="fw-bold ">
+                <img
+                  src={gameIcon}
+                  alt="icon"
+                  className="short-icon trending-icon  me-4 "
+                />
+                Game
+              </li>
+              <li className="fw-bold ">
+                <img
+                  src={newsIcon}
+                  alt="icon"
+                  className="short-icon trending-icon  me-4 "
+                />
+                News
+              </li>
+              <li className="fw-bold ">
+                <img
+                  src={sportIcon}
+                  alt="icon"
+                  className="short-icon trending-icon  me-4 "
+                />
+                Sport
+              </li>
+            </ul>
+            <hr />
+            <ul>
+              <li className="fw-bold ">
+                <img
+                  src={browserIcon}
+                  alt="icon"
+                  className="short-icon trending-icon  me-4 "
+                />
+                Browse Channel
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-
-  
-      
-    </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
